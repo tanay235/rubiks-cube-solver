103e7c9acd99f9af6294acaadc4aedbe8046904d
Tanay_Mittal <142823713+tanay235@users.noreply.github.com>
2026-03-08 11:02:30 +0530

    Rubiks Cube get move

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -20,3 +20,46 @@ char RubiksCube::getColorLetter(COLOR color) {
             return 'O';
     }
 }
+/*
+ * Describe a move using an index
+ */
+string RubiksCube::getMove(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return "L";
+        case MOVE::LPRIME:
+            return "L'";
+        case MOVE::L2:
+            return "L2";
+        case MOVE::R:
+            return "R";
+        case MOVE::RPRIME:
+            return "R'";
+        case MOVE::R2:
+            return "R2";
+        case MOVE::U:
+            return "U";
+        case MOVE::UPRIME:
+            return "U'";
+        case MOVE::U2:
+            return "U2";
+        case MOVE::D:
+            return "D";
+        case MOVE::DPRIME:
+            return "D'";
+        case MOVE::D2:
+            return "D2";
+        case MOVE::F:
+            return "F";
+        case MOVE::FPRIME:
+            return "F'";
+        case MOVE::F2:
+            return "F2";
+        case MOVE::B:
+            return "B";
+        case MOVE::BPRIME:
+            return "B'";
+        case MOVE::B2:
+            return "B2";
+    }
+}
