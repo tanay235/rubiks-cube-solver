75fd3fa490cbe6a0f681b0c8677b46748577416f
Tanay_Mittal <142823713+tanay235@users.noreply.github.com>
2026-03-08 11:03:11 +0530

    Move

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -63,3 +63,47 @@ string RubiksCube::getMove(MOVE ind) {
             return "B2";
     }
 }
+
+/*
+ * Perform a move operation on using a Move index.
+ */
+RubiksCube &RubiksCube::move(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return this->l();
+        case MOVE::LPRIME:
+            return this->lPrime();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->r();
+        case MOVE::RPRIME:
+            return this->rPrime();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->u();
+        case MOVE::UPRIME:
+            return this->uPrime();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->d();
+        case MOVE::DPRIME:
+            return this->dPrime();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->f();
+        case MOVE::FPRIME:
+            return this->fPrime();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->b();
+        case MOVE::BPRIME:
+            return this->bPrime();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
