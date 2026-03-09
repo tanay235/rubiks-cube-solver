b3072cee6fd290515836e6f94c065ae9c80a4a5d
Tanay_Mittal <142823713+tanay235@users.noreply.github.com>
2026-03-09 20:15:47 +0530

    Invert a move

--- Model/RubiksCube.cpp
+++ Model/RubiksCube.cpp
@@ -107,3 +107,46 @@ RubiksCube &RubiksCube::move(MOVE ind) {
             return this->b2();
     }
 }
+/*
+ * Invert a move.
+ */
+RubiksCube &RubiksCube::invert(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return this->lPrime();
+        case MOVE::LPRIME:
+            return this->l();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->rPrime();
+        case MOVE::RPRIME:
+            return this->r();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->uPrime();
+        case MOVE::UPRIME:
+            return this->u();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->dPrime();
+        case MOVE::DPRIME:
+            return this->d();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->fPrime();
+        case MOVE::FPRIME:
+            return this->f();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->bPrime();
+        case MOVE::BPRIME:
+            return this->b();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
