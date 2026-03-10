c1feb92c5e9de94ec06bb701584791c9e629513c
Tanay_Mittal <142823713+tanay235@users.noreply.github.com>
2026-03-10 23:49:53 +0530

    making rubikscube

---
+++ Model/RubiksCube.h
@@ -0,0 +1,12 @@
+#ifndef RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+#define RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+
+#include "bits/stdc++.h"
+
+using namespace std;
+/**
+ * A base class for all Rubik's Cube Model. There are various representation for Rubik's Cube.
+ * Each one has it's own special ways of definitions. This class provides a shared functionality
+ * between all models.
+ * We'll benchmark all models and observe which one is better for performance.
+ */
